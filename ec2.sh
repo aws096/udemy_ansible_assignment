c7fad8bcad9a8b11f6c10bbc0c436d17859a03c2
Lubomir Stanko <lst@whitestein.com>
2017-10-20 13:25:07 +0200

    Some corrections and provision ELB

--- ec2.sh
+++ ec2.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-ansible-playbook -i inventories playbook.ec2-start.yml --private-key=MyEC2KeyPair.pem -u ec2-user
+terminate() {
+    echo "Terminating EC2 instances"
+    ansible-playbook -i inventories/ec2.py playbook.ec2-terminate.yml
+}
+start() {
+    echo "Starting EC2 instances"
+    ansible-playbook -i inventories/ec2.py playbook.ec2-start.yml
+}
+deploy() {
+    echo "Deploying webapp"
+    ansible-playbook -i inventories/ec2.py playbook.yml --private-key=MyEC2KeyPair.pem -u ec2-user
 
-ansible-playbook -i inventories/ec2.py playbook.yml --private-key=MyEC2KeyPair.pem -u ec2-user
+}
+case "$1" in
+    terminate)
+        terminate
+        ;;
+    start)
+        start
+        ;;
+    deploy)
+        deploy
+        ;;
+    *)
+        echo "Usage: ./$(basename "$0") {terminate|start|deploy}"
+        exit 1
+        ;;
+esac
